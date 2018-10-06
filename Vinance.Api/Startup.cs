164e311d8585d37be5596a4e1a52c062a60ba0c9
Peter Vincze <peter.vincze.dev@gmail.com>
2018-10-06 11:52:18 +0200

    added Data project

--- Vinance.Api/Startup.cs
+++ Vinance.Api/Startup.cs
@@ -14,20 +14,18 @@ namespace Vinance.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
