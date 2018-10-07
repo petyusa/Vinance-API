4d9336a7d2930af4d99808178efa7811ffb9e6af
Peter Vincze <peter.vincze.dev@gmail.com>
2018-10-07 14:54:34 +0200

    added HeaderValidation Attribute

---
+++ Vinance.Api/ActionFilters/HeaderValidationFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Vinance.Api.ActionFilters
+{
+    public class HeaderValidationFilterAttribute : IResourceFilter
+    {
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            if (context.HttpContext.Request.Method.Equals("put", StringComparison.OrdinalIgnoreCase) ||
+                context.HttpContext.Request.Method.Equals("post", StringComparison.OrdinalIgnoreCase))
+            {
+                if (context.HttpContext.Request.ContentType != "application/json")
+                {
+                     throw new Exception("wrong content-type");
+                }
+
+            }
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            return;
+        }
+    }
+}
