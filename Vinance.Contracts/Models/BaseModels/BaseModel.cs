fd5896f458f338c53957e11974cd9f0255959c85
Peter Vincze <peter.vincze.dev@gmail.com>
2018-10-07 14:55:14 +0200

    implemented Equals and GetHashcode on Base model

--- Vinance.Contracts/Models/BaseModels/BaseModel.cs
+++ Vinance.Contracts/Models/BaseModels/BaseModel.cs
@@ -3,5 +3,21 @@
     public class BaseModel
     {
         public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as BaseModel;
+            return other?.Id == Id;
+        }
+
+        public bool Equals(BaseModel other)
+        {
+            return Id == other?.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
     }
 }
